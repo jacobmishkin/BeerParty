fa7109eee849d4bd49504b1b4380dbafbf2fceed
Jacob Mishkin <user@MacBook-Pro.local>
2017-10-05 14:54:53 -0500

    git init

---
+++ index.js
@@ -0,0 +1,8 @@
+import { AppRegistry } from 'react-native';
+import App from './app/components/App';
+
+AppRegistry.registerComponent('beerParty', () => App);
+
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
